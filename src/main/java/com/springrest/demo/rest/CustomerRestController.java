66165dc884ebbb7c9ce670af77874ca14e1e2f12
arun <arunv04@gmail.com>
2020-03-10 15:19:59 +0530

    Added add customer/ Post functionality

--- src/main/java/com/springrest/demo/rest/CustomerRestController.java
+++ src/main/java/com/springrest/demo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,4 +42,22 @@ public class CustomerRestController {
 			
 			return theCustomer;
 	  }	 
+	  
+	  
+	//Post Mapping for add new customer
+	  @PostMapping("/customers")
+	  public Customer addCustomer(@RequestBody Customer theCustomer) {
+		  
+		   //Also just in case the pass an id in JSON ....set id to 0
+		   //this is way to save of new item ..instead of using update
+		  
+		   theCustomer.setId(0);
+		  	
+		   customerService.saveCustomer(theCustomer);
+		   
+		   return theCustomer;
+	  }
+	  
+	  
+	  
 }
